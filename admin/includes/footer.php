c283bb12c56ea7c98512a26626a5be0518b67042
Neverlanders <vanhoutte.tom@gmail.com>
2025-01-16 10:24:03 +0100

    einde strippen index.php

---
+++ admin/includes/footer.php
@@ -0,0 +1,28 @@
+<footer>
+    <div class="footer clearfix mb-0 text-muted">
+        <div class="float-start">
+            <p>2023 &copy; Mazer</p>
+        </div>
+        <div class="float-end">
+            <p>Crafted with <span class="text-danger"><i class="bi bi-heart-fill icon-mid"></i></span>
+                by <a href="https://saugi.me">Saugi</a></p>
+        </div>
+    </div>
+</footer>
+</div>
+</div>
+<script src="assets/static/js/components/dark.js"></script>
+<script src="assets/extensions/perfect-scrollbar/perfect-scrollbar.min.js"></script>
+
+
+<script src="assets/compiled/js/app.js"></script>
+
+
+
+<!-- Need: Apexcharts -->
+<script src="assets/extensions/apexcharts/apexcharts.min.js"></script>
+<script src="assets/static/js/pages/dashboard.js"></script>
+
+</body>
+
+</html>
---
+++ admin/includes/widget.php
@@ -0,0 +1,63 @@
+</div>
+<div class="col-12 col-lg-3">
+    <div class="card">
+        <div class="card-body py-4 px-4">
+            <div class="d-flex align-items-center">
+                <div class="avatar avatar-xl">
+                    <img src="./assets/compiled/jpg/1.jpg" alt="Face 1">
+                </div>
+                <div class="ms-3 name">
+                    <h5 class="font-bold">John Duck</h5>
+                    <h6 class="text-muted mb-0">@johnducky</h6>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="card">
+        <div class="card-header">
+            <h4>Recent Messages</h4>
+        </div>
+        <div class="card-content pb-4">
+            <div class="recent-message d-flex px-4 py-3">
+                <div class="avatar avatar-lg">
+                    <img src="./assets/compiled/jpg/4.jpg">
+                </div>
+                <div class="name ms-4">
+                    <h5 class="mb-1">Hank Schrader</h5>
+                    <h6 class="text-muted mb-0">@johnducky</h6>
+                </div>
+            </div>
+            <div class="recent-message d-flex px-4 py-3">
+                <div class="avatar avatar-lg">
+                    <img src="./assets/compiled/jpg/5.jpg">
+                </div>
+                <div class="name ms-4">
+                    <h5 class="mb-1">Dean Winchester</h5>
+                    <h6 class="text-muted mb-0">@imdean</h6>
+                </div>
+            </div>
+            <div class="recent-message d-flex px-4 py-3">
+                <div class="avatar avatar-lg">
+                    <img src="./assets/compiled/jpg/1.jpg">
+                </div>
+                <div class="name ms-4">
+                    <h5 class="mb-1">John Dodol</h5>
+                    <h6 class="text-muted mb-0">@dodoljohn</h6>
+                </div>
+            </div>
+            <div class="px-4">
+                <button class='btn btn-block btn-xl btn-outline-primary font-bold mt-3'>Start Conversation</button>
+            </div>
+        </div>
+    </div>
+    <div class="card">
+        <div class="card-header">
+            <h4>Visitors Profile</h4>
+        </div>
+        <div class="card-body">
+            <div id="chart-visitors-profile"></div>
+        </div>
+    </div>
+</div>
+</section>
+</div>
