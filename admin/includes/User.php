af0b0510e93b9534c4fe6f72224ee9af74fc9fe1
Neverlanders <vanhoutte.tom@gmail.com>
2025-01-20 11:52:04 +0100

    einde object toepassing

--- admin/includes/User.php
+++ admin/includes/User.php
@@ -10,17 +10,37 @@ class User
     public $first_name;
     public $last_name;
     //methods
-    public static function find_this_query($sql, $values=[]){
+    public static function find_this_query($sql, $values = []){
         global $database;
-        $result = $database->query($sql, $values);
-        return $result;
+        $result = $database->query($sql,$values);
+        $the_object_array = [];
+        while($row = mysqli_fetch_assoc($result)){
+            $the_object_array[] = self::instantie($row);
+        }
+        return $the_object_array;
     }
+
+    public static function instantie($result){
+        $the_object = new self();
+        foreach($result as $the_attribute => $value){
+            if($the_object->has_the_attribute($the_attribute)){
+                $the_object->$the_attribute = $value;
+            }
+        }
+        return $the_object;
+    }
+    public function has_the_attribute($the_attribute){
+        $object_properties = get_object_vars($this);
+        return array_key_exists($the_attribute, $object_properties);
+    }
+
     public static function find_all_users(){
         return self::find_this_query("SELECT * FROM users");
     }
     public static function find_user_by_id($user_id){
         //binden van parameters= ($user_id)=PREPARED STATEMENTS
-        return self::find_this_query("SELECT * FROM users WHERE id=?",[$user_id]);
+        $result = self::find_this_query("SELECT * FROM users WHERE id=?",[$user_id]);
+        return !empty($result) ? array_shift($result): false;
     }
 
 }
--- admin/users.php
+++ admin/users.php
@@ -14,66 +14,27 @@ require_once("includes/content-top.php");
             <table class="table table-striped" id="table1">
                 <thead>
                 <tr>
-	                <?php
-	                //dynamisch kolommen ophalen uit database
-//                    $user = new User();
-//                    $result = $user->find_all_users();
-
-	                  $result = User::find_all_users();
-		               $fields = mysqli_fetch_fields($result);
-						foreach($fields as $field){
-	                        echo "<th>" . htmlspecialchars(str_replace('_', ' ', $field->name), ENT_QUOTES, 'UTF-8') . "</th>";
-	                    }
-	                ?>
-<!--	                <th>Klantnr</th>-->
-<!--	                <th>username</th>-->
-<!--	                <th>paswoord</th>-->
-<!--	                <th>Voornaam</th>-->
-<!--	                <th>Familienaam</th>-->
+	                <th>Klantnr</th>
+	                <th>username</th>
+	                <th>paswoord</th>
+	                <th>Voornaam</th>
+	                <th>Familienaam</th>
                 </tr>
                 </thead>
                 <tbody>
-                   <?php if(mysqli_num_rows($result)>0):?>
-                     <?php while($row = mysqli_fetch_array($result)):?>
+                    <?php $users = User::find_all_users(); ?>
+                     <?php foreach($users as $user):?>
 	                   <tr>
-		                   <td><?= htmlspecialchars($row['id'], ENT_QUOTES, 'UTF-8') ?></td>
-		                   <td><span><img height="40" width="40" class="avatar me-3" src="../admin/assets/static/images/faces/8.jpg" alt=""></span><?= htmlspecialchars($row['username'], ENT_QUOTES, 'UTF-8') ?></td>
-		                   <td><?= htmlspecialchars($row['password'], ENT_QUOTES, 'UTF-8') ?></td>
-		                   <td><?= htmlspecialchars($row['first_name'], ENT_QUOTES, 'UTF-8') ?></td>
-		                   <td><?= htmlspecialchars($row['last_name'], ENT_QUOTES, 'UTF-8') ?></td
+		                   <td><?= $user->id; ?></td>
+		                   <td><span><img height="40" width="40" class="avatar me-3" src="../admin/assets/static/images/faces/8.jpg" alt=""></span><?= $user->username; ?></td>
+		                   <td><?= $user->password;?></td>
+		                   <td><?= $user->first_name;?></td>
+		                   <td><?= $user->last_name; ?></td
 	                   </tr>
-                     <?php endwhile;?>
-                   <?php else: ?>
-
-                   <?php endif; ?>
+                     <?php endforeach;?>
 
                 </tbody>
             </table>
-	        <?php
-                echo "ophalen van 1 users procedureel met array";
-				$result = User::find_user_by_id(1);
-				if(mysqli_num_rows($result)>0){
-					while($row = mysqli_fetch_array($result)){
-						echo "<br> id:".$row['id']." ".$row['username']."<br>";
-					}
-				}else{
-					echo "0 results";
-				}
-
-	        ?>
-
-	        <?php
-	            echo "object oriented van 1 user";
-				$result = User::find_user_by_id(1);
-				$user = new User();
-				$row = mysqli_fetch_assoc($result);
-				$user->id= $row['id'];
-				$user->username= $row['username'];
-				$user->password= $row['password'];
-				$user->first_name= $row['first_name'];
-				$user->last_name=$row['last_name'];
-	            echo "<br> id:" . $user->id . " : " . $user->username;
-	        ?>
         </div>
     </div>
 
