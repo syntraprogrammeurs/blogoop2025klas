be5700c83ff1133c28fd8af479bc4e7da7d38dac
Neverlanders <vanhoutte.tom@gmail.com>
2025-01-21 11:19:39 +0100

    fc

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
-<?php
-echo "frontend";
-?>
+<?php require_once("includes/header.php"); ?>
+<a class="text-success display-3 text-center font-bold" href="login.php"><i class="bi bi-upload"></i></a>
+</body>
+</html>
